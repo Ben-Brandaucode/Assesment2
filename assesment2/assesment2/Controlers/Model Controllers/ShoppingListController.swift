e766441a5c366d84315bdb32248ce9355294237e
Benjamin Brandau <codingbrandau@gmail.com>
2020-07-31 10:02:11 -0600

    Controller add

---
+++ assesment2/assesment2/Controlers/Model Controllers/ShoppingListController.swift
@@ -0,0 +1,9 @@
+//
+//  ShoppingListController.swift
+//  assesment2
+//
+//  Created by Ben Brandau Brandau on 7/31/20.
+//  Copyright © 2020 Ben Brandau. All rights reserved.
+//
+
+import Foundation
